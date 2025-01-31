d230a43e8ac470fae4c868e1eadc2473d40dda21
Abraham <saulabraham881@gmail.com>
2025-01-31 01:07:52 -0600

    i create some code of week 4

---
+++ CSE_110/W04_Project_Word_Puzzle.py
@@ -0,0 +1,11 @@
+def word_guessing_game():
+    hidden_word = "mosiah
+    for letter in hidden_word:
+    if (letter == " "):
+        print(" ", end="")
+        # comment: 
+    # end if
+    print("_", end="")
+
+
+
